6bd7478e4be2b11f918311576e24ab04593d2e70
Dagmara <dagmara.szczepanczyk@gmail.com>
2020-02-06 11:10:11 +0100

    add responsive HeroImage component, remove router as it was no longer useful

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,33 +1,53 @@
 <template>
-  <div class="searchWrapper">
-      <input
-      id="search"
-      name="search"
-      v-model="searchValue"
-      @input="handleInput"
-      />
-    </div>
+  <input
+  id="search"
+  name="search"
+  :value="value"
+  @input="handleChange"
+  />
 </template>
 
 <script>
 export default {
   name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
 };
 
 </script>
 
 <style lang="scss" scoped>
-.searchWrapper {
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    width: 300px;
-  }
 
   input {
-      background: none;
+    margin-top: 50px;
+    display: flex;
+    width: 250px;
+    color: white;
+    text-align: center;
+    font-size: 18px;
+    background: none;
     height: 30px;
     border: 0;
-    border-bottom: 1px solid black;
+    border-bottom: 1px solid white;
+    transition: box-shadow .3s ease-out;
+
+    @media (min-width: 1024px){
+      font-weight: 400;
+    }
   }
+
+  input:focus {
+      outline: none;
+      box-shadow: 0 10px 20px -8px rgba(255,255,255, .5);
+  }
+
 </style>
